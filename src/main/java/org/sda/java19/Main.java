7dfb4eff7d1d84d0b1d8a42ad73715cfd61b8b70
Cepblu-tyt <sergei.oksanen@gmail.com>
2022-11-21 20:40:20 +0200

    Warehouse

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,7 +1,23 @@
 package org.sda.java19;
 
+/**
+ *  Exercise-2
+ *
+ * 2. Warehouse
+ * a. User should be able to: add, display all of the details, update, delete an item
+ * b. Use composition and collections (The warehouse has products/items)
+ * c. Add possibility to display summaries, like sum of all of the products, their prices.
+ * d. *Add possibility to update number of items in a specific way, e.g.:
+ * “pliers:30”
+ * “scissors:+4”
+ *
+ * @author Sergei Oksanen
+ *
+ */
 public class Main {
     public static void main(String[] args) {
+
+
 
     }
 }
