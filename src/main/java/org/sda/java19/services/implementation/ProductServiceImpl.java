3320ff54256293309c4499d2c90674a2a4d2c078
Cepblu-tyt <sergei.oksanen@gmail.com>
2022-12-04 19:59:38 +0200

    Warehouse exercise

--- src/main/java/org/sda/java19/services/implementation/ProductServiceImpl.java
+++ src/main/java/org/sda/java19/services/implementation/ProductServiceImpl.java
@@ -21,7 +21,7 @@ public class ProductServiceImpl implements ProductService {
     public void addProduct(Product product) {
         try {
             Warehouse warehouse = warehouseService.getWarehouse();
-            List<Product> products = new ArrayList<>(Arrays.asList(product));
+            List<Product> products = new ArrayList<>(Arrays.asList(warehouse.getProducts().toArray(new Product[0])));
             products.add(product);
             warehouse.setProducts(products);
             warehouseService.updateWarehouse(warehouse);
